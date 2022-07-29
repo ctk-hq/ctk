58aba1da61c939f8c452d41e4521cabdaad413eb
Samuel Rowe <samuelrowe1999@gmail.com>
2022-07-29 11:20:21 +0530

    feat(frontend): updated spacing in general tab of network modal

--- services/frontend/src/components/Modal/network/General.tsx
+++ services/frontend/src/components/Modal/network/General.tsx
@@ -1,11 +1,18 @@
+import { styled } from "@mui/joy";
 import TextField from "../../global/FormElements/TextField";
+
+const Root = styled("div")`
+  display: flex;
+  flex-direction: column;
+  row-gap: ${({ theme }) => theme.spacing(1)};
+`;
 
 const General = () => {
   return (
-    <>
+    <Root>
       <TextField label="Entry name" name="entryName" />
       <TextField label="Network name" name="networkName" />
-    </>
+    </Root>
   );
 };
 
