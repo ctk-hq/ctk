5f527e7fc62c5aceec31ca62edbc1cb36ba34962
Artem Golub <artemgolub@gmail.com>
2026-02-13 13:10:19 +0200

    cleanup

--- services/backend/alembic/env.py
+++ services/backend/alembic/env.py
@@ -16,7 +16,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "app")))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
